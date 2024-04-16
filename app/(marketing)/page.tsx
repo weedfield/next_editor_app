1478ed25b583a20d2ab782505d9d1b73af3a86ae
鍋田　勇貴 <01059620@CC6239.local>
2024-04-17 01:05:34 +0900

    LP (WIP)

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import  Footer  from "./_components/footer";
+import  Heading  from "./_components/heading";
+import Heroes from "./_components/heroes";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-between text-center gap-y-8 flex-1 px-6">
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default MarketingPage
+
+// #ff5702
